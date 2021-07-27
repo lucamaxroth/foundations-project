ec641abce626a1e78b15df32da251143bf65f255
lucamaxroth <luca.m.roth@icloud.com>
2021-07-27 16:24:44 +0200

    Implement initial functionality

--- project/static/script.js
+++ project/static/script.js
@@ -1,3 +1,75 @@
-function warnUser(){
-    alert("You pushed a button!")
+var projectList = document.getElementById('project-list');
+
+
+function expandCollapseToggle(i) {
+    let projectElement = projectList.getElementsByClassName("project")[i];
+    let shortDescription = projectElement.getElementsByClassName("short-description")[0];
+    let longDescription = projectElement.getElementsByClassName("long-description")[0];
+    let button = projectElement.getElementsByClassName("button")[0];
+
+    if (shortDescription.style.display == "block") {
+        shortDescription.style.display = "none";
+        longDescription.style.display = "block";
+        button.innerHTML = "Collapse";
+    } else {
+        shortDescription.style.display = "block";
+        longDescription.style.display = "none";
+        button.innerHTML = "Expand";
+    }
 }
+
+function expandForm() {
+    let addProject = document.getElementById("add-project");
+    let button = document.getElementById("add-project-button");
+
+    if (addProject.style.display == "none") {
+        addProject.style.display = "block";
+        button.innerHTML = "Collapse Form";
+    } else {
+        addProject.style.display = "none";
+        button.innerHTML = "Add another Project";
+    }
+}
+
+async function loadProjects() {
+    let projectsFile;
+
+    try {
+        let request = await fetch("/static/projects.json", {
+            method: "GET"
+        });
+        projectsFile = await request.json();
+    } catch (error) {
+        projectList.innerHTML = "An Error occured. Try to relaod the page.";
+        throw new Error(error);
+    }
+
+    projectsFile.forEach(function (project, i) {
+        let clone = document.getElementById("project-template").cloneNode(true);
+        clone.removeAttribute("id");
+
+        let projectTitle = clone.getElementsByClassName("project-title")[0];
+        let projectPicture = clone.getElementsByClassName("project-picture")[0];
+        let shortDescription = clone.getElementsByClassName("short-description")[0];
+        let longDescription = clone.getElementsByClassName("long-description")[0];
+        let button = clone.getElementsByClassName("button")[0];
+
+        projectTitle.innerHTML = project.projectTitle;
+        projectPicture.src = project.projectPicture;
+        shortDescription.innerHTML = project.shortDescription;
+        longDescription.innerHTML = project.longDescription;
+
+        button.onclick = function() {
+            expandCollapseToggle(i);
+        }
+
+        projectList.appendChild(clone);
+    });
+
+}
+
+loadProjects();
+
+// function warnUser(){
+//     alert("You pushed a button!")
+// }
--- project/website.py
+++ project/website.py
@@ -1,5 +1,7 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect
+from uuid import uuid4
+import json
 
 app = Flask(__name__)
 
@@ -9,7 +11,32 @@ app.config.from_pyfile("config.py")
 
 @app.route('/')
 def index():
-    return render_template('index.html', page_title="My website")
+    return render_template('index.html', page_title="My little project")
+
+@app.route('/form', methods=['POST'])
+def form():
+
+
+    with open('project/static/projects.json', 'r', encoding='utf-8') as projects_file:
+        projects_file = json.load(projects_file)
+
+    file_name = str(uuid4()) + ".jpeg"
+    image_file = request.files["project-picture"]
+    image_file.save("./project/static/img/" + file_name)
+
+    project = {
+        "projectTitle": request.form['project-title'],
+        "projectPicture": "/static/img/" + file_name,
+        "shortDescription": request.form['short-description'],
+        "longDescription": request.form['long-description']
+    }
+    projects_file.append(project)
+
+    with open('project/static/projects.json', 'w+', encoding='utf-8') as out_file:
+        json.dump(projects_file, out_file, indent='  ')
+
+    return redirect('/', code="302")
+
 
 
 if __name__ == "__main__":
