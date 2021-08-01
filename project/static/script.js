2a628082db8810ba351182347d25dfc2fa218724
lucamaxroth <luca.m.roth@icloud.com>
2021-08-01 13:44:10 +0200

    Add comments, clean up code and update CSS

--- project/static/script.js
+++ project/static/script.js
@@ -1,12 +1,19 @@
+//assigns the project-list to a global variable
 var projectList = document.getElementById('project-list');
 
-
+/**
+ * Expands or collapses a project with a specific index given through the button.
+ * @param {Number} i Index of project
+ */
 function expandCollapseToggle(i) {
+    // Assignes the whole project element to a variable
     let projectElement = projectList.getElementsByClassName("project")[i];
+    // Assignes the elements to variables
     let shortDescription = projectElement.getElementsByClassName("short-description")[0];
     let longDescription = projectElement.getElementsByClassName("long-description")[0];
     let button = projectElement.getElementsByClassName("button")[0];
 
+    // Checks the elements shortDescription is shown and adjusts the style
     if (shortDescription.style.display == "block") {
         shortDescription.style.display = "none";
         longDescription.style.display = "block";
@@ -18,10 +25,15 @@ function expandCollapseToggle(i) {
     }
 }
 
-function expandForm() {
+/**
+ * Expands and collapses the form.
+ */
+function expandCollapseForm() {
+    // Assignes form and button to a variable
     let addProject = document.getElementById("add-project");
     let button = document.getElementById("add-project-button");
 
+    // Checks if the form is shown and adjusts the style
     if (addProject.style.display == "none") {
         addProject.style.display = "block";
         button.innerHTML = "Collapse Form";
@@ -31,45 +43,55 @@ function expandForm() {
     }
 }
 
+/**
+ * Loads all projects from the json to generate the websites main content.
+ */
 async function loadProjects() {
     let projectsFile;
 
+    // Loads the json from the server and displays and error message if unsuccessful
     try {
+        // Send request for the project file to the server
         let request = await fetch("/static/projects.json", {
             method: "GET"
         });
+        // assigns the requests json content to a variabale
         projectsFile = await request.json();
     } catch (error) {
         projectList.innerHTML = "An Error occured. Try to relaod the page.";
         throw new Error(error);
     }
 
+    // Loops through all projects in the json
     projectsFile.forEach(function (project, i) {
+        // Clones the project-template element
         let clone = document.getElementById("project-template").cloneNode(true);
+        // Removes the id from the clone
         clone.removeAttribute("id");
 
+        // Assigns the child elements to a variable
         let projectTitle = clone.getElementsByClassName("project-title")[0];
         let projectPicture = clone.getElementsByClassName("project-picture")[0];
         let shortDescription = clone.getElementsByClassName("short-description")[0];
         let longDescription = clone.getElementsByClassName("long-description")[0];
         let button = clone.getElementsByClassName("button")[0];
 
-        projectTitle.innerHTML = project.projectTitle;
+        // Generates the html content from the json
+        projectTitle.innerText = project.projectTitle;
         projectPicture.src = project.projectPicture;
-        shortDescription.innerHTML = project.shortDescription;
-        longDescription.innerHTML = project.longDescription;
+        shortDescription.innerText = project.shortDescription;
+        longDescription.innerText = project.longDescription;
 
-        button.onclick = function() {
+        // Defines the onclick actions of the button
+        button.onclick = function () {
             expandCollapseToggle(i);
         }
 
+        // Attaches the processed project to the project-list in the document
         projectList.appendChild(clone);
     });
 
 }
 
+// Loads all projects
 loadProjects();
-
-// function warnUser(){
-//     alert("You pushed a button!")
-// }
--- project/website.py
+++ project/website.py
@@ -13,30 +13,33 @@ app.config.from_pyfile("config.py")
 def index():
     return render_template('index.html', page_title="My little project")
 
+
 @app.route('/form', methods=['POST'])
 def form():
-
-
+    # This function writes the forms input into a json.
+    # Therefore the picture is given a random name.
+    # The picture is referenced with its files location.
     with open('project/static/projects.json', 'r', encoding='utf-8') as projects_file:
         projects_file = json.load(projects_file)
 
-    file_name = str(uuid4()) + ".jpeg"
-    image_file = request.files["project-picture"]
-    image_file.save("./project/static/img/" + file_name)
+        file_name = str(uuid4()) + ".jpeg"
+        image_file = request.files["project-picture"]
+        image_file.save("./project/static/img/" + file_name)
 
-    project = {
-        "projectTitle": request.form['project-title'],
-        "projectPicture": "/static/img/" + file_name,
-        "shortDescription": request.form['short-description'],
-        "longDescription": request.form['long-description']
-    }
-    projects_file.append(project)
+        project = {
+            "projectTitle": request.form['project-title'],
+            "projectPicture": "/static/img/" + file_name,
+            "shortDescription": request.form['short-description'],
+            "longDescription": request.form['long-description']
+        }
 
+        projects_file.append(project)
+
+    # Dumps the old json and overwrites it with the newly created json.
     with open('project/static/projects.json', 'w+', encoding='utf-8') as out_file:
         json.dump(projects_file, out_file, indent='  ')
 
     return redirect('/', code="302")
-
 
 
 if __name__ == "__main__":
